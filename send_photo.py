081b4ba64c0c3196e3ab5f6e50a411566afa5c16
Aferokript <alberto.cardinale@bk.ru>
2026-03-31 21:34:53 +0300

    добавил логику и таймер

--- send_photo.py
+++ send_photo.py
@@ -1,9 +1,17 @@
 import telegram
+import os
+import time
 
 
-def send_photo(bot_token, chat_id, photo_path):
+def send_photo(bot_token, chat_id, interval):
     bot = telegram.Bot(token=bot_token)
-
-    with open(photo_path, 'rb') as photo:
-        bot.send_photo(chat_id=chat_id, photo=photo)
+    timer = interval * 60 * 60
+    while True:
+        folder = 'APOD'
+        files = os.listdir(folder)
+        for file in files:
+            photo_path = os.path.join(folder, file)
+            with open(photo_path, 'rb') as photo:
+                bot.send_photo(chat_id, photo)
+                time.sleep(timer)
 
